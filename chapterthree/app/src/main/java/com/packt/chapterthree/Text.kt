e2cf54a7900299016468345fb26fc1dabd1edea4
Harun Wangereka <harunwangereka@gmail.com>
2023-06-25 13:19:41 +0200

    few fixes

--- chapterthree/app/src/main/java/com/packt/chapterthree/Text.kt
+++ chapterthree/app/src/main/java/com/packt/chapterthree/Text.kt
@@ -1,12 +1,24 @@
 package com.packt.chapterthree
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun PacktPublishing(bookName: String) {
-    Text(text = "Title of the book is: $bookName")
+    Text(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+            .background(Color.Green),
+        text = "Title of the book is: $bookName"
+    )
 }
 
 @Preview(showBackground = true)
