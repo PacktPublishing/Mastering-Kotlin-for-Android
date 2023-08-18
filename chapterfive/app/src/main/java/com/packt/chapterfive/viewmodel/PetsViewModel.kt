6e5e8761bea26b55b14b42ec8c6201104f736ac5
Harun Wangereka <harunwangereka@gmail.com>
2023-08-18 20:19:48 +0300

    setup

--- chapterfive/app/src/main/java/com/packt/chapterfive/viewmodel/PetsViewModel.kt
+++ chapterfive/app/src/main/java/com/packt/chapterfive/viewmodel/PetsViewModel.kt
@@ -2,10 +2,10 @@ package com.packt.chapterfive.viewmodel
 
 import androidx.lifecycle.ViewModel
 import com.packt.chapterfive.data.PetsRepository
-import com.packt.chapterfive.data.PetsRepositoryImpl
 
-class PetsViewModel: ViewModel() {
-    private val petsRepository: PetsRepository = PetsRepositoryImpl()
+class PetsViewModel(
+    private val petsRepository: PetsRepository
+): ViewModel() {
 
     fun getPets() = petsRepository.getPets()
 }
--- chapterfive/app/src/main/java/com/packt/chapterfive/views/PetList.kt
+++ chapterfive/app/src/main/java/com/packt/chapterfive/views/PetList.kt
@@ -10,12 +10,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.packt.chapterfive.viewmodel.PetsViewModel
+import org.koin.androidx.compose.koinViewModel
 
 @Composable
 fun PetList(modifier: Modifier) {
-    val petsViewModel: PetsViewModel = viewModel()
+    val petsViewModel: PetsViewModel = koinViewModel()
     LazyColumn(
         modifier = modifier
     ) {
