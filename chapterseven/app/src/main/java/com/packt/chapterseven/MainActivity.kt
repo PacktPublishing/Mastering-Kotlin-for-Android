4619d262dc5e5c444961f478aae0c0cd956875f1
Harun Wangereka <harunwangereka@gmail.com>
2023-09-01 11:14:14 +0300

    refactoring

--- chapterseven/app/src/main/java/com/packt/chapterseven/MainActivity.kt
+++ chapterseven/app/src/main/java/com/packt/chapterseven/MainActivity.kt
@@ -3,11 +3,9 @@ package com.packt.chapterseven
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.ExperimentalMaterial3Api
 import com.packt.chapterseven.navigation.AppNavigation
 import com.packt.chapterseven.ui.theme.ChapterSevenTheme
 
-@OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
