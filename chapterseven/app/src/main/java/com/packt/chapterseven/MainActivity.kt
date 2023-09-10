0485f4b340ed89462a279061f5aeeb2bc6f2c61e
Harun Wangereka <harunwangereka@gmail.com>
2023-09-10 23:44:22 +0300

    finalize on chapter 7

--- chapterseven/app/src/main/java/com/packt/chapterseven/MainActivity.kt
+++ chapterseven/app/src/main/java/com/packt/chapterseven/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.PermanentDrawerSheet
 import androidx.compose.material3.PermanentNavigationDrawer
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
@@ -106,15 +107,17 @@ class MainActivity : ComponentActivity() {
                 if (navigationType == NavigationType.NavigationDrawer) {
                     PermanentNavigationDrawer(
                         drawerContent = {
-                            PetsNavigationDrawer(
-                                onFavoriteClicked = {
-                                    navController.navigate(Screens.FavoritePetsScreen.route)
+                            PermanentDrawerSheet {
+                                PetsNavigationDrawer(
+                                    onFavoriteClicked = {
+                                        navController.navigate(Screens.FavoritePetsScreen.route)
 
-                                },
-                                onHomeClicked = {
-                                    navController.navigate(Screens.PetsScreen.route)
-                                }
-                            )
+                                    },
+                                    onHomeClicked = {
+                                        navController.navigate(Screens.PetsScreen.route)
+                                    }
+                                )
+                            }
                         }
                     ) {
                         AppNavigationContent(
