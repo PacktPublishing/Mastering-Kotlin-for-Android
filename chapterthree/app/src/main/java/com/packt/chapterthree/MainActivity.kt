5755f34befabb99d1846bc85c894ca8c160b6cf8
Harun Wangereka <harunwangereka@gmail.com>
2024-01-15 18:19:28 +0100

    names to reflect chapter 3

--- chapterthree/app/src/main/java/com/packt/chapterthree/MainActivity.kt
+++ chapterthree/app/src/main/java/com/packt/chapterthree/MainActivity.kt
@@ -10,13 +10,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.packt.chapterthree.ui.theme.ChaptertwoTheme
+import com.packt.chapterthree.ui.theme.ChapterThreeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ChaptertwoTheme {
+            ChapterThreeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -40,7 +40,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    ChaptertwoTheme {
+    ChapterThreeTheme {
         Greeting("Android")
     }
 }
--- chapterthree/app/src/main/java/com/packt/chapterthree/ui/theme/Theme.kt
+++ chapterthree/app/src/main/java/com/packt/chapterthree/ui/theme/Theme.kt
@@ -38,7 +38,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun ChaptertwoTheme(
+fun ChapterThreeTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
