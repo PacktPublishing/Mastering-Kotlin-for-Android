d2b6d729d031e679f23d34c171808e4fd5a4f807
Harun Wangereka <harunwangereka@gmail.com>
2024-01-16 22:09:40 +0100

    update as version and deps

--- chapterfive/app/build.gradle.kts
+++ chapterfive/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.packt.chapterfive"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.packt.chapterfive"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
